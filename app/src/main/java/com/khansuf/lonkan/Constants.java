58a3c670e02bb39b4e914fb5290ce5219c300eae
tan cash <zannatnira8@gmail.com>
2023-09-18 19:51:50 +0600

    final version

---
+++ app/src/main/java/com/khansuf/lonkan/Constants.java
@@ -0,0 +1,34 @@
+package com.khansuf.lonkan;
+
+public class Constants
+{
+    public static final String APP_ID = "nirob2";
+    public static String ApplovinSDK = "tJHCynaM8hDvWdAEcgHhwwNoPSrhcAOBf1QhRTGgtgklFdiRM97c1N5s9iZXw1bpzT8fNSm7G0_k7miIr6bqNa";
+    public static final int APP_VERSION = 2;
+    public static final Boolean SHOW_DRAWER = true;
+    public static final String Ni_Kajol_DEV_URL = "https://t.me/Dev_BD";
+    public static final String ONESIGNAL_APP_ID = "6a56f944-2253-41a8-85ee-f306de081045";
+    public static final String EXTRA_PARAMS = "";
+    public static final String PAKAGE_NAME = "com.khansuf.lonkan";
+
+    public static final String TASK_APP_INDEX = "0";
+    public static final String HOME_PAGE = "index.php";
+    public static final String HOST_DOMAIN = "developerbd.xyz";
+
+    public static final String BASE_URL = "https://" + HOST_DOMAIN + "/" + APP_ID;
+    public static final String APP_ROOT = BASE_URL + "/app";
+    public static final String HOME_URL = APP_ROOT + "/" + HOME_PAGE;
+
+    public static final String APP_DATA_API_URL = APP_ROOT + "/api/app-data.php";
+    public static final String TASK_DATA_API_URL = APP_ROOT + "/api/task-data.php";
+    public static final String TASK_DATA_API_URL_SR = APP_ROOT + "/api/srcreatortaskdataapi.php";
+    public static final String TASK_DATA_API_URL_SR_ON = APP_ROOT + "/api/srcreatortaskdataonapi.php";
+    public static final String TASK_AUTH_API_URL = APP_ROOT + "/api/task-authh.php";
+    public static final String TASK_AUTH_API_URL_SR = APP_ROOT + "/api/task_auth_sr.php";
+    public static final String TASK_AUTH_API_URL_SR_on = APP_ROOT + "/api/task_auth_sr_on.php";
+    public static final String INVALID_CLICK_API_URL = APP_ROOT + "/api/invalid.php";
+
+
+}
+
+
